aac883dba25397ae263bf9d6c5ed52fce8288ef3
Jack Nemitz <jack.nemitz@yahoo.com>
2020-08-16 22:21:32 -0500

    changes

--- main.js
+++ main.js
@@ -27,3 +27,25 @@ function closeFullscreen() {
     document.msExitFullscreen();
   }
 }
+
+
+/*-----------------------------------------------------------------*/
+
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  var ampm = h >= 12 ? 'pm' : 'am';
+  h = h % 12;
+  h = h ? h : 12; // the hour '0' should be '12'
+  document.getElementById('txt').innerHTML =
+  h + ":" + m;
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
