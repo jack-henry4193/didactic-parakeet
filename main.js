e5dbc213828ba295762c5972e3b415d1ed6d9175
Jack Nemitz <nemit031@umn.edu>
2020-09-03 23:22:13 -0500

    added functionality for stock news

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 /* Get the documentElement (<html>) to display the page in fullscreen */
 var elem = document.documentElement;
 
@@ -142,9 +141,9 @@ function getWeather() {
             descrip = data.current.weather[0].description;
             icon = data.current.weather[0].icon;
             temp = Math.round(temp);
-            document.getElementById('tmp').innerHTML = temp+"°";
+            document.getElementById('tmp').innerHTML = temp + "°";
             document.getElementById('des').innerHTML = descrip;
-            document.getElementById('icon-now').src = "/icons/" + icon + ".png";
+            document.getElementById('icon-now').src = "icons/" + icon + ".png";
             forcast1 = data.hourly[2].temp;
             forcast1 = Math.round(forcast1);
             document.getElementById('forcast1').innerHTML = forcast1 + "°";
@@ -155,11 +154,11 @@ function getWeather() {
             forcast3 = Math.round(forcast3);
             document.getElementById('forcast3').innerHTML = forcast3 + "°";
             icon1 = data.hourly[2].weather[0].icon;
-            document.getElementById('icon1').src = "/icons/" + icon1 + ".png";
+            document.getElementById('icon1').src = "icons/" + icon1 + ".png";
             icon2 = data.hourly[4].weather[0].icon;
-            document.getElementById('icon2').src = "/icons/" + icon2 + ".png";
+            document.getElementById('icon2').src = "icons/" + icon2 + ".png";
             icon3 = data.hourly[6].weather[0].icon;
-            document.getElementById('icon3').src = "/icons/" + icon3 + ".png";
+            document.getElementById('icon3').src = "icons/" + icon3 + ".png";
         });
 }
 
@@ -169,4 +168,97 @@ setInterval(getWeather, 60000);
 
 /*-------------------------------------------------------*/
 
+const Fin_key = "bt8q75f48v6ulu345cig"
+const AV_key = "FASLSWE4EQRXW4OT"
+
+function getStock() {
+    symbol1 = "AAPL"
+    fetch("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + symbol1 + "&apikey=" + AV_key)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            symbol = data["Global Quote"]["01. symbol"]
+            price = data["Global Quote"]["05. price"]
+            price = Math.round(price * 100) / 100;
+            chg = data["Global Quote"]["09. change"]
+            chg = Math.round(chg * 100) / 100;
+            pchg = data["Global Quote"]["10. change percent"]
+            pchg = String(pchg).substring(0, 6);
+            pchg = Math.round(parseFloat(pchg) * 100) / 100;
+            document.getElementById('1').innerHTML = symbol + ' ' + price + ' ' + chg + ' ' + pchg + '%';
+        });
+    symbol2 = "TSLA"
+    fetch("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + symbol2 + "&apikey=" + AV_key)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            symbol = data["Global Quote"]["01. symbol"]
+            price = data["Global Quote"]["05. price"]
+            price = Math.round(price * 100) / 100;
+            chg = data["Global Quote"]["09. change"]
+            chg = Math.round(chg * 100) / 100;
+            pchg = data["Global Quote"]["10. change percent"]
+            pchg = String(pchg).substring(0, 6);
+            pchg = Math.round(parseFloat(pchg) * 100) / 100;
+            document.getElementById('2').innerHTML = symbol + ' ' + price + ' ' + chg + ' ' + pchg + '%';
+        });
+    symbol3 = "AMZN"
+    fetch("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + symbol3 + "&apikey=" + AV_key)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            symbol = data["Global Quote"]["01. symbol"]
+            price = data["Global Quote"]["05. price"]
+            price = Math.round(price * 100) / 100;
+            chg = data["Global Quote"]["09. change"]
+            chg = Math.round(chg * 100) / 100;
+            pchg = data["Global Quote"]["10. change percent"]
+            pchg = String(pchg).substring(0, 6);
+            pchg = Math.round(parseFloat(pchg) * 100) / 100;
+            document.getElementById('3').innerHTML = symbol + ' ' + price + ' ' + chg + ' ' + pchg + '%';
+        });
+    symbol4 = "MSFT"
+    fetch("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + symbol4 + "&apikey=" + AV_key)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            symbol = data["Global Quote"]["01. symbol"]
+            price = data["Global Quote"]["05. price"]
+            price = Math.round(price * 100) / 100;
+            chg = data["Global Quote"]["09. change"]
+            chg = Math.round(chg * 100) / 100;
+            pchg = data["Global Quote"]["10. change percent"]
+            pchg = String(pchg).substring(0, 6);
+            pchg = Math.round(parseFloat(pchg) * 100) / 100;
+            document.getElementById('4').innerHTML = symbol + ' ' + price + ' ' + chg + ' ' + pchg + '%';
+        });
+    symbol5 = "NDAQ"
+    fetch("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + symbol5 + "&apikey=" + AV_key)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            symbol = data["Global Quote"]["01. symbol"]
+            price = data["Global Quote"]["05. price"]
+            price = Math.round(price * 100) / 100;
+            chg = data["Global Quote"]["09. change"]
+            chg = Math.round(chg * 100) / 100;
+            pchg = data["Global Quote"]["10. change percent"]
+            pchg = String(pchg).substring(0, 6);
+            pchg = Math.round(parseFloat(pchg) * 100) / 100;
+            document.getElementById('5').innerHTML = symbol + ' ' + price + ' ' + chg + ' ' + pchg + '%';
+        });
+}
+
+getStock();
+setInterval(getStock(), 60100);
 
