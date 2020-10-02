88b767a5bb3fb1706abb5c0b85c5978bae548443
terminalPoltergeist <nemit031@umn.edu>
2020-10-02 17:20:37 -0500

    added configuration file

---
+++ v 2.x.x/electron-app/app_config.js
@@ -0,0 +1,20 @@
+/*
+// Go to https://openweathermap.org/api and create a free account. Get an api key and add it here.
+var weth_key = ''
+// find your latitude and longitude and add it here.
+var lat = 
+var lon = 
+// Go to https://www.alphavantage.co and get an api key and add it here.
+var AV_key = ""
+
+// do not alter this line
+var config_path = './app_config.js'
+*/
+
+// Go to https://openweathermap.org/api and create a free account. Get an api key and add it here.
+var weth_key = 'e265de32d5883ec3609745c6b02072b3'
+// find your latitude and longitude and add it here.
+var lat = 44.72
+var lon = -96.28
+// Go to https://www.alphavantage.co and get an api key and add it here.
+var AV_key = "FASLSWE4EQRXW4OT"
