3fb7ca98941fbf84f431da61d73783890adf9014
terminalPoltergeist <nemit031@umn.edu>
2020-10-02 18:07:34 -0500

    updated readme

--- v 2.x.x/electron-app/app_config.js
+++ v 2.x.x/electron-app/app_config.js
@@ -1,4 +1,3 @@
-/*
 // Go to https://openweathermap.org/api and create a free account. Get an api key and add it here.
 var weth_key = ''
 // find your latitude and longitude and add it here.
@@ -9,4 +8,3 @@ var AV_key = ""
 
 // do not alter this line
 var config_path = './app_config.js'
-*/
